1f3738f7060d2eefb19ad17fc9972f6432a19e90
QaraByte <pchelpkz16@gmail.com>
2020-01-21 23:54:55 +0600

    1) Создал место (Bitmap) для рисования; 2) Создал функцию DrawLineInt(), которая рисует 11 линий; 3) Линии рисуются по 2-м таймерам.

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(674, 330);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 386);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(916, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel1
+            // 
+            this.statusLabel1.Name = "statusLabel1";
+            this.statusLabel1.Size = new System.Drawing.Size(10, 17);
+            this.statusLabel1.Text = ".";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.BackColor = System.Drawing.Color.White;
+            this.txtInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtInfo.Location = new System.Drawing.Point(680, 0);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInfo.Size = new System.Drawing.Size(227, 375);
+            this.txtInfo.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 408);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox txtInfo;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap b;
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+            b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(b);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            //DrawLineInt(100, 100, 300, 150);
+            //DrawLineInt(300, 150, 200, 350);
+            button1.Enabled = false;
+        }
+
+        Pen p = new Pen(Color.Blue);
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = 0;
+        Random rnd = new Random();
+
+        public void DrawLineInt(int c)
+        {
+            switch (c)
+            {
+                case 1: x1 = 1; y1 = 300; x2 = rnd.Next(30, 50); y2 = 300; break;           // ->>
+                case 2: x1 = x2; y1 = y2; x2 = x1; y2 = rnd.Next(200, 300); break;          // /\
+                case 3: x1 = x2; y1 = y2; x2 = x1 + rnd.Next(x1 + 20, x1 + 50); break;      // ->>
+                case 4: x1 = x2; y1 = y2; x2 = x1; y2 = 300; break;                         // \/
+                case 5: x1 = x2; y1 = y2; x2 = rnd.Next(x1, x1 + 20); y2 = 300; break;      // ->>
+                case 6: x1 = x2; y1 = y2; x2 = x1; y2 = rnd.Next(200, 300); break;          // /\
+                case 7: x1 = x2; y1 = y2; x2 = rnd.Next(x1 + 20, x1 + 50); y2 = y1; break;  // ->>
+                case 8: x1 = x2; y1 = y2; x2 = x1; y2 = 300; break;                         // \/
+                case 9: x1 = x2; y1 = y2; x2 = rnd.Next(x1, x1 + 20); y2 = 300; break;      // ->>
+                case 10: x1 = x2; y1 = y2; x2 = x1; y2 = rnd.Next(200, 300); break;         // /\
+                case 11: x1 = x2; y1 = y2; x2 = rnd.Next(x1+20, x1 + 50); y2 = y1; break;   // ->>
+                case 12: x1 = x2; y1 = y2; x2 = x1; y2 = 300; break;                        // \/
+            }
+
+            g.DrawLine(p, x1, y1, x2, y2);
+            pictureBox1.Image = b;
+            
+            statusLabel1.Text = "Координаты: x1=" + x1 + "; y1=" + y1 + "; x2=" + x2 + "; y2=" + y2;
+            txtInfo.Text += "Шаг: " + c1 + Environment.NewLine;
+            txtInfo.Text += "Координаты: x1=" + x1 + "; y1=" + y1 + "; x2=" + x2 + "; y2=" + y2 + Environment.NewLine;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        int c1 = 1;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DrawLineInt(c1);
+
+            c1++;
+
+            if (c1 > 11)
+            {
+                c1 = 1;
+                button1.Enabled = true;
+                timer1.Enabled = false;
+                return;
+            }
+                
+            timer1.Enabled = false;
+            timer2.Enabled = true;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            DrawLineInt(c1);
+            c1++;
+            timer1.Enabled = true;
+            timer2.Enabled = false;
+        }
+    }
+}
